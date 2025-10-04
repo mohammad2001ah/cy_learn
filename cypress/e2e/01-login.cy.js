6ef562d0afa3ec0540a786c7e0424bb2441484dc
mohammad2001ah <mah.2001.ah@gmail.com>
2025-10-04 19:07:35 +0300

    mohammad commit

--- cypress/e2e/01-login.cy.js
+++ cypress/e2e/01-login.cy.js
@@ -4,6 +4,15 @@ describe('Login Page', () => {
 
   it("TC #1 :should be able to login with valid email and password", () => {
     cy.visit("/");
+    cy.visit("/");
+    cy.visit("/");
+    cy.visit("/");
+    cy.visit("/");
+    cy.visit("/");
+    cy.visit("/");
+    cy.visit("/");
+    cy.visit("/");
+    cy.visit("/");
   });
-  
+  //mnbjhbhjnm
 });
