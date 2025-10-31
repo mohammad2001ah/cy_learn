858916c3e334b075073fbee696b784de86f5f3c6
mohammad2001ah <mah.2001.ah@gmail.com>
2025-10-31 19:50:48 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const mongoose = require('mongoose');
-const authenticateToken = require('./middlewares/auth');
+const authenticateToken = require('./middlewares/auth.js');
 const cors = require('cors');
 
 //Import Routes
