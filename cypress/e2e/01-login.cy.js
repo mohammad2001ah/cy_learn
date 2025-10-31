96e877aea195138094f56e139d03ca1c1a855717
mohammad2001ah <mah.2001.ah@gmail.com>
2025-10-31 18:14:58 +0300

    fix

--- cypress/e2e/01-login.cy.js
+++ cypress/e2e/01-login.cy.js
@@ -4,15 +4,6 @@ describe('Login Page', () => {
 
   it("TC #1 :should be able to login with valid email and password", () => {
     cy.visit("/");
-    cy.visit("/");
-    cy.visit("/");
-    cy.visit("/");
-    cy.visit("/");
-    cy.visit("/");
-    cy.visit("/");
-    cy.visit("/");
-    cy.visit("/");
-    cy.visit("/");
+    
   });
-  //mnbjhbhjnm
 });
